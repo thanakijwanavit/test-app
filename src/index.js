3dbf8c7960d0503bbda5d571fa3ca41b801b6bb6
Nic Wanavit <nwanavit@gmail.com>
2024-09-01 12:14:20 +0700

    clear project and add index html

--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import BookShelf from './components/BookShelf';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<BookShelf />);
-
-
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
